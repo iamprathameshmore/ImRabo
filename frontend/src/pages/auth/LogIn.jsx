e5933bed280a1b65c105d403df6f48f6607cd62b
iamprathameshmore <iamprathameshmore07@gmail.com>
2025-02-11 21:52:53 +0530

    modified login screen

--- frontend/src/pages/auth/LogIn.jsx
+++ frontend/src/pages/auth/LogIn.jsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
 import { toast, ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
 import Logo from "../../components/logo";
 
 function LogIn() {
