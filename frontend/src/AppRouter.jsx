9872207bed07ff8d7533236fefb6f3e3698a26cb
iamprathameshmore <iamprathameshmore07@gmail.com>
2025-02-11 22:17:59 +0530

    update approuter imports

--- frontend/src/AppRouter.jsx
+++ frontend/src/AppRouter.jsx
@@ -3,9 +3,10 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 import ImraboHome from './pages/Home';
 
-import Verification from './pages/auth/verification'; 
-import SignUp from './pages/auth/signup';
+
 import Login from './pages/auth/loginpage';
+import Verification from './pages/auth/verifiication';
+import SignUp from './pages/auth/SignUp';
 
 function AppRouter() {
   const router = createBrowserRouter([
